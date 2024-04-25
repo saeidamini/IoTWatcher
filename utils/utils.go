737dd7e67d26a1bf634a9dc21968cc472696f442
Saeid Amini <amini.com@gmail.com>
2024-04-25 09:03:31 +0200

    Feat: HTTP JSON response.
    Add support return error response as JSON format.

--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,23 @@
 package utils
 
 import (
+	"encoding/json"
 	"errors"
+	"net/http"
 )
 
 var (
 	ErrDeviceNotFound = errors.New("device not found")
 )
+
+type ErrorJSON struct {
+	Message string `json:"message"`
+	Status  int    `json:"status,omitempty"`
+}
+
+func ErrorJSONFormat(w http.ResponseWriter, err string, code int) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(ErrorJSON{Message: err, Status: code})
+}
